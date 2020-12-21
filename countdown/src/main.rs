bb7711b48b11a19b2db344708cf0579453426c5b
Tim Schumacher <tim@timakro.de>
2020-12-21 22:38:10 +0100

    Parallelize with rayon

--- countdown/src/main.rs
+++ countdown/src/main.rs
@@ -1,7 +1,5 @@
 use std::collections::{HashSet,HashMap};
-use std::sync::mpsc;
-use std::thread;
-use num_cpus;
+use rayon::prelude::*;
 
 fn calc_results(results: &mut HashSet<i32>, ns: Vec<i32>) {
     for i in 0..ns.len() {
@@ -68,27 +66,18 @@ fn get_all_boards() -> Vec<[i32; 6]> {
 }
 
 fn main() {
-    let (tx, rx) = mpsc::channel();
-    let all_boards: Vec<[i32; 6]> = get_all_boards();
-    for boards in all_boards.chunks(all_boards.len() / num_cpus::get()) {
-        let boards = boards.to_owned();
-        let tx = tx.clone();
-        thread::spawn(move || {
-            for board in boards {
-                let mut results: HashSet<i32> = HashSet::new();
-                calc_results(&mut results, board.to_vec());
-                tx.send(results).unwrap();
-            }
-        });
-    }
-    drop(tx);
+    let boards: Vec<[i32; 6]> = get_all_boards();
+
+    let results: Vec<HashSet<i32>> = boards.par_iter().map(|board| {
+        let mut results: HashSet<i32> = HashSet::new();
+        calc_results(&mut results, board.to_vec());
+        results
+    }).collect();
 
     let mut commonness: HashMap<i32,i32> = HashMap::new();
-    for results in rx {
-        for result in results {
-            if 100 <= result && result <= 999 {
-                *commonness.entry(result).or_insert(0) += 1;
-            }
+    for result in results.into_iter().flatten() {
+        if 100 <= result && result <= 999 {
+            *commonness.entry(result).or_insert(0) += 1;
         }
     }
 
